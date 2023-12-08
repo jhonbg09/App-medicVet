b02f3203fbacc1ba5ac97bb9dbf529d3e19f14bc
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-08 12:31:42 -0500

    Creando componente Pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,8 +1,13 @@
-
+import Pacientes from "./Pacientes";
 const ListadoPacientes = () => {
     return (
-        <div className="w-1/2 lg:-3/5">
-            <h2 >Listado Pacientes</h2>
+        <div className="md:w-1/2 lg:-3/5 md:h-screen overflow-y-scroll">
+            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+            <p className="text-xl mt-5 text-center mb-10">Administra tus {' '}<span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
+            <Pacientes/>
+            <Pacientes/>
+            <Pacientes/>
+            <Pacientes/>
         </div>
     );
 }
---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,30 @@
+
+const Pacientes = () => {
+    return (
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+                <p className="font-bold text-gray-700 mb-3 uppercase">
+                    Nombre:{' '}
+                    <span className="font-normal normal-case">Hook</span>
+                </p>
+                <p className="font-bold text-gray-700 mb-3 uppercase">
+                    Propietario:{' '}
+                    <span className="font-normal normal-case">Jhon Betancourth</span>
+                </p>
+                <p className="font-bold text-gray-700 mb-3 uppercase">
+                    Email:{' '}
+                    <span className="font-normal normal-case">jhon.betancourth03@gmail.com</span>
+                </p>
+                <p className="font-bold text-gray-700 mb-3 uppercase">
+                    Fecha Alta:{' '}
+                    <span className="font-normal normal-case">10 de Diciembre 2023</span>
+                </p>
+                <p className="font-bold text-gray-700 mb-3 uppercase">
+                    Sintomas{' '}
+                    <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum leo turpis, ornare vel magna eget, auctor dictum nisl. Suspendisse congue nisl quis congue elementum. Fusce rhoncus, turpis ut condimentum lobortis, sem nisi elementum elit, vitae pharetra nisl risus a dolor. Sed pellentesque, elit et bibendum pretium, nulla metus consectetur diam, ut tincidunt ex leo ut mi. Etiam at luctus mi, aliquet feugiat elit. Fusce ultricies erat nec odio pellentesque rutrum. Cras at massa eget lectus egestas semper. Donec ac semper sapien. Nunc tortor augue, laoreet ac mi eget, egestas vehicula lorem. </span>
+                </p>
+
+            </div>
+    );
+}
+
+export default Pacientes;
