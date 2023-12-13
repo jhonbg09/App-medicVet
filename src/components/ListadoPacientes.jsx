b1530e90e2cd1167c2f89251398fb6348b198224
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-13 16:44:23 -0500

    Renderizado condicional en componente listaPacinetes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,20 +2,31 @@
 /* eslint-disable react/prop-types */
 import Pacientes from "./Pacientes";
 const ListadoPacientes = ({ paciente }) => {
-    // console.log(paciente)
-    return (
-        <div className="md:w-1/2 lg:-3/5 md:h-screen overflow-y-scroll">
-            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-            <p className="text-xl mt-5 text-center mb-10">Administra tus {' '}<span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
-            {paciente.map((paciente )=>(
-                <Pacientes 
-                key={paciente.id}
-                paciente={paciente}
-
-                />
-                ))}
-        </div>
-    );
-}
+  return (
+    <div className="md:w-1/2 lg:-3/5 md:h-screen overflow-y-scroll">
+      {/* cons esta line de codigo voy a colocar el renderizado condicional */}
+      {paciente && paciente.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 text-center mb-10">
+            Administra tus{" "}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+          {paciente.map((paciente) => (
+            <Pacientes key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+        <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 text-center mb-10">
+            Comienza agregando pacientes{" "}
+            <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default ListadoPacientes;
